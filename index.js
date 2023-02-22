a28b0e2269b50b197260ec14236ce777035ffe32
Jessica RRuffels <jlruffels@yahoo.com>
2023-02-22 12:12:10 -0500

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2
+let num2 = 31
+let multiply = (num1*num2);
 
-// Write your code here
+let num3 = 16
+let num4 = 4
+let mod = (num3/num4);
+
+let max = Math.max(1,2,10,20);
+
+let random = 1
+function random() {
+    return Math.random();
+}
